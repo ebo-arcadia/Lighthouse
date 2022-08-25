23be0d2c51e2f4f34392bea2a059e2c59cb98b2c
Ebo Arcadia <yiboli.coding@gmail.com>
2022-08-24 20:51:17 -0400

    Initial Commit

---
+++ Lighthouse/LighthouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LighthouseApp.swift
+//  Lighthouse
+//
+//  Created by Ebo Lee on 8/24/22.
+//
+
+import SwiftUI
+
+@main
+struct LighthouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
