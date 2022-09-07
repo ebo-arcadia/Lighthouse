85e90e05adc6be2ad760ed3ac6a3f90031dfa586
Ebo Arcadia <yiboli.coding@gmail.com>
2022-09-06 20:57:57 -0400

    build basic landmark detail view

--- Lighthouse/ContentView.swift
+++ Lighthouse/ContentView.swift
@@ -9,11 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Welcome to lighthouse")
-            .fontWeight(.bold)
+        VStack {
+            MapView()
+                .frame(height: 250)
+                .ignoresSafeArea(edges: .top)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("The Unfinished Church")
+                    .fontWeight(.bold)
+                    .font(.title)
+                    .foregroundColor(.blue)
+                HStack {
+                    Text("St. George")
+                        .font(.subheadline)
+                        .fontWeight(.black)
+                        .foregroundColor(Color.green)
+                    Spacer()
+                    Text("Bermuda")
+                        .font(.subheadline)
+                        .fontWeight(.black)
+                        .foregroundColor(Color.orange)
+                }
+                
+                Divider()
+                
+                Text("About The Unfinished Church")
+                    .font(.title2)
+                Text("beautiful")
+            }
             .padding()
-            .font(.title)
-            .foregroundColor(.blue)
+            
+            Spacer()
+        }
     }
 }
 
